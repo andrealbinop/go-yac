d8ab511cf0efd86952d3ee1b5d5d3ddfb1aeed67
Andre Albino Pereira <andreptb@gmail.com>
2018-11-24 15:04:58 -0200

    Adds handler to properly close `io.Reader` in `loader.Data.Load()`
    If `io.Reader` provided by `loader.Reader` also implements `io.Closer`, `loader.Data`
    now tries to properly close the resource, returning any possible failures
    when doing so.
    
    Resolves #4

--- pkg/loader/data_test.go
+++ pkg/loader/data_test.go
@@ -4,13 +4,12 @@ import (
 	"errors"
 	"github.com/andrealbinop/go-yac/internal/mocks"
 	"github.com/stretchr/testify/assert"
-	"strings"
+	"io"
 	"testing"
 )
 
 const (
 	valLocation = "mock location"
-	valData     = "mock data"
 )
 
 func TestDataFailedToRead(t *testing.T) {
@@ -29,11 +28,12 @@ func TestDataFailedToRead(t *testing.T) {
 
 func TestDataFailedToParse(t *testing.T) {
 	reader := mocks.Reader{}
-	mockReader := strings.NewReader(valData)
-	reader.On("Read", valLocation).Return(mockReader, nil)
+	ioReader := &mocks.IOReadCloser{}
+	reader.On("Read", valLocation).Return(ioReader, nil)
+	ioReader.On("Close").Return(nil)
 	parser := mocks.Parser{}
 	expectedError := errors.New("mock read error")
-	parser.On("Parse", mockReader).Return(nil, expectedError)
+	parser.On("Parse", ioReader).Return(nil, expectedError)
 	loader := Data{
 		Location: valLocation,
 		Reader:   &reader,
@@ -41,19 +41,51 @@ func TestDataFailedToParse(t *testing.T) {
 	}
 	cfg, err := loader.Load()
 	reader.AssertExpectations(t)
+	ioReader.AssertExpectations(t)
 	assert.Nil(t, cfg)
 	assert.Equal(t, expectedError, err)
 }
 
-func TestDataLoad(t *testing.T) {
+func TestDataFailedToClose(t *testing.T) {
 	reader := mocks.Reader{}
-	mockReader := strings.NewReader(valData)
-	reader.On("Read", valLocation).Return(mockReader, nil)
+	ioReader := &mocks.IOReadCloser{}
+	expectedError := errors.New("mock close error")
+	reader.On("Read", valLocation).Return(ioReader, nil)
+	ioReader.On("Close").Return(expectedError)
+	parser := mocks.Parser{}
+	parser.On("Parse", ioReader).Return(make(map[string]interface{}), nil)
+	loader := Data{
+		Location: valLocation,
+		Reader:   &reader,
+		Parser:   &parser,
+	}
+	_, err := loader.Load()
+	reader.AssertExpectations(t)
+	ioReader.AssertExpectations(t)
+	assert.Equal(t, expectedError, err)
+}
+
+func TestDataLoadForIOReader(t *testing.T) {
+	ioReader := &mocks.IOReader{}
+	testDataLoad(t, ioReader)
+	ioReader.AssertExpectations(t)
+}
+
+func TestDataLoadForIOReadCloser(t *testing.T) {
+	ioReader := &mocks.IOReadCloser{}
+	ioReader.On("Close").Return(nil)
+	testDataLoad(t, ioReader)
+	ioReader.AssertExpectations(t)
+}
+
+func testDataLoad(t *testing.T, ioReaderResponse io.Reader) {
+	reader := mocks.Reader{}
+	reader.On("Read", valLocation).Return(ioReaderResponse, nil)
 	parser := mocks.Parser{}
 	database := map[string]interface{}{
 		"prop.1": "value.1",
 	}
-	parser.On("Parse", mockReader).Return(database, nil)
+	parser.On("Parse", ioReaderResponse).Return(database, nil)
 	loader := Data{
 		Location: valLocation,
 		Reader:   &reader,
